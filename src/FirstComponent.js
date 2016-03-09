8ef26b1cb6e9e3aeecd3f806a924102f8dd5480c
Amir Raisszadeh <Amir.raisszadeh@iag.com.au>
2016-03-09 16:47:56 +1100

    init commit

---
+++ src/FirstComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+export class FirstComponent extends Component {
+  render() {
+    return (
+      <div>FirstComponent</div>
+    )
+  }
+}
